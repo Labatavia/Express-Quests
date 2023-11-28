b3380337aafad303161649eab47a9b616de358bd
Maxime Dumont <maximedumont44800@gmail.com>
2023-11-28 16:21:08 +0100

    quete03

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -13,7 +13,7 @@ const getUsers = (req, res) => {
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   database
-    .query("select * from movies where id = ?", [id])
+    .query("select * from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
@@ -43,8 +43,33 @@ const postUsers = (req, res) => {
       res.sendStatus(500);
     });
 };
+
+const updateUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+      [ firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
 module.exports = {
   getUsers,
   getUserById,
   postUsers,
+  updateUsers,
 };
